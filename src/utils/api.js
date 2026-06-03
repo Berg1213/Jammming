312022a819bad91536c67050858be1ff760a5083
Mitch <206980813+Berg1213@users.noreply.github.com>
2026-06-03 14:09:15 -0700

    Add API endpoint functions

--- src/utils/api.js
+++ src/utils/api.js
@@ -12,4 +12,50 @@ const search = async (query) => {
   return data;
   }
 
-  export {search}
+const getUserID = async () => {
+  const accessToken = localStorage.getItem('access_token');
+  const response = await fetch(`https://api.spotify.com/v1/me`, {
+       headers: {
+        'Authorization': `Bearer ${accessToken}`,
+      }
+    });
+  const data = await response.json();
+  return data;
+  }
+
+const createPlaylist = async (name, description) => {
+  const accessToken = localStorage.getItem('access_token');
+  const response = await fetch(`https://api.spotify.com/v1/me/playlists`, {
+       method: 'POST',
+       headers: {
+        'Authorization': `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+       },
+       body: JSON.stringify({
+        "name": name,
+        "description": description,
+        "public": false,
+      }),
+    });
+  const data = await response.json();
+  return data;
+  }
+
+const addTracksToPlaylist = async (id, tracks, position) => {
+  const accessToken = localStorage.getItem('access_token');
+  const response = await fetch(`https://api.spotify.com/v1/playlists/${id}/items`, {
+      method: 'POST',
+      headers: {
+      'Authorization': `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+      "position": position,
+      "uris":tracks
+    }),
+  });
+const data = await response.json();
+return data;
+}
+
+export {search, getUserID, createPlaylist, addTracksToPlaylist}
